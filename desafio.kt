00548cf52c26346b87bac2f161e8fae9e6233413
Anchieta Acacio <protonsnet@gmail.com>
2023-09-25 09:43:57 -0300

    Desafio em Kotlin, DIO

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,66 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { 
+    BASICO, 
+    INTERMEDIARIO, 
+    AVANÇADO 
+}
 
-class Usuario
+data class Usuario(val nome: String, val idade: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+val disciplina = listOf(
+    	ConteudoEducacional("FMC 1", 90),
+        ConteudoEducacional("Calculo 1", 60),
+        ConteudoEducacional("FMC 2", 60),
+        ConteudoEducacional("Probabilidade", 60),
+    )
+
+
+fun getConteudo(): List<ConteudoEducacional> = disciplina
+
+class Formacao(var nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+       inscritos.add(usuario)
+        
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario = listOf(
+        Usuario("José", 30),
+        Usuario("Maria", 25),
+        Usuario("Carlos", 20),
+        Usuario("Pedro", 38),
+    )
+    
+    val disc = getConteudo()
+    
+	var formacao = Formacao("TI", disc)
+    
+    val u1 = usuario[1]
+    val u2 = usuario[0]
+    
+    formacao.matricular(u1)
+    formacao.matricular(u2)
+
+    println("Usuários cadastrados:")
+    usuario.forEach{
+        println(it.nome)
+    }
+    
+    println("\nDisciplinas")
+    disciplina.forEach{
+        println(it.nome)
+    }
+    
+    println("\nFormação")
+    formacao.inscritos.forEach{
+        println(it.nome)
+    }
+    
 }
